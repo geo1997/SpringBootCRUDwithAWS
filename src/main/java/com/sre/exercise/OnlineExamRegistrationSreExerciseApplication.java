d02e7c71f16aac89822556a6c996ce9cbd4a891c
IT18148428_K.G.G.H.Silva <it18148428@my.sliit.lk>
2022-04-01 13:37:41 +0530

    Project Initializing

---
+++ src/main/java/com/sre/exercise/OnlineExamRegistrationSreExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.sre.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamRegistrationSreExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamRegistrationSreExerciseApplication.class, args);
+	}
+
+}
